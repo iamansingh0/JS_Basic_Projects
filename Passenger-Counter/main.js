abc169d0f0d8038219ca2fbfbf6c6438be10cf4a
Aman Kumar Singh <amankumar7355197337@gmail.com>
2022-06-28 17:21:20 +0530

    day2

--- Passenger-Counter/main.js
+++ Passenger-Counter/main.js
@@ -1,1 +1,19 @@
 let count = 0;
+const counter = document.querySelector("#count");
+const info = document.querySelector(".para");
+const result = document.getElementById("result");
+// increament the count variable whenever the button is clicked
+function increament() {
+    count++;
+    counter.innerHTML = count;
+    info.style.visibility = "hidden";
+}
+
+// create a function save(), which logs out the count when it's called
+
+function save() {
+    result.innerHTML = count;
+    count = 0;
+    counter.innerHTML = count;
+    info.style.visibility = "visible";
+}
