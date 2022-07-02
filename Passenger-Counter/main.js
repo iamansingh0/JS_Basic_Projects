714171c1845ed431f687114181e0b7fee404ce5b
Aman Kumar Singh <amankumar7355197337@gmail.com>
2022-07-02 15:18:26 +0530

    layout

--- Passenger-Counter/main.js
+++ Passenger-Counter/main.js
@@ -2,7 +2,9 @@ let count = 0;
 const counter = document.querySelector("#count");
 const info = document.querySelector(".para");
 const result = document.getElementById("result");
+
 // increament the count variable whenever the button is clicked
+
 function increament() {
     count++;
     counter.innerHTML = count;
