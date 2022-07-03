58ad9f6cf479989c61f06cbfd0a010f733445374
Aman Kumar Singh <amankumar7355197337@gmail.com>
2022-07-03 19:57:54 +0530

    finished

--- 2_SimpleCalculator/main.js
+++ 2_SimpleCalculator/main.js
@@ -4,49 +4,101 @@ const calculator = {
   waitingForSecondOperand: false,
   op2: null,
   operator: null,
+  secondOperand: "0",
 };
 
 function input(digit) {
-  const { displayValue, waitingForSecondOperand } = calculator;
-  if (waitingForSecondOperand === true) {
-    calculator.displayValue += digit;
-    calculator.op2 = digit;
-    calculator.waitingForSecondOperand = false;
-    console.log(calculator)
-  } else {
-    calculator.displayValue =
-      displayValue === "0" ? digit : displayValue + digit;
-      console.log(calculator)
-  }
+  const { displayValue } = calculator;
+  calculator.displayValue = displayValue === "0" ? digit : displayValue + digit;
+  console.log(calculator);
 }
 
-function inputDecimal(dot) {
+function secondInput(digit) {
+  calculator.op2 = null;
+  const { secondOperand } = calculator;
+  calculator.secondOperand =
+    secondOperand === "0" ? digit : secondOperand + digit;
+  calculator.displayValue += digit;
+  console.log(calculator);
+}
+
+function FirstInputDecimal(dot) {
   // If the `displayValue` property does not contain a decimal point
   if (!calculator.displayValue.includes(dot)) {
     calculator.displayValue += dot;
-    console.log(calculator)
+    console.log(calculator);
+  }
+}
+
+function SecondInputDecimal(dot) {
+  if (!calculator.secondOperand.includes(dot)) {
+    calculator.secondOperand += dot;
+    calculator.displayValue += dot;
+    console.log(calculator);
   }
 }
 
 function OperatorHandling(op) {
-  const { op1, displayValue, operator, op2 } = calculator;
-  const inputValue = parseFloat(displayValue);
+  const { op1, displayValue, operator, op2, secondOperand } = calculator;
+  calculator.waitingForSecondOperand = false;
 
-  if (op1 == null && !isNaN(inputValue)) {
+  let inputValue, secondInputValue;
+
+  if (op1 == null) {
+    inputValue = parseFloat(displayValue);
     calculator.op1 = inputValue;
-    console.log(calculator)
+    // console.log(calculator)
   } else if (operator != null) {
-    const result = calculate(op1, inputValue, operator);
-    console.log(result)
-    calculator.displayValue = result + operator;
+    secondInputValue = parseFloat(secondOperand);
+    console.log(`second input: ${secondInputValue}`);
+    calculator.op2 = secondInputValue;
+    const result = calculate(op1, calculator.op2, operator);
+    console.log(result);
+    calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
     calculator.op1 = result;
-    console.log(calculator);
+    calculator.secondOperand = "0";
+    // console.log(calculator);
   }
 
   calculator.waitingForSecondOperand = true;
-  calculator.displayValue += op
+  calculator.displayValue += op;
   calculator.operator = op;
   console.log(calculator);
+}
+
+function resetCalculator() {
+  calculator.displayValue = "0";
+  calculator.secondOperand = "0";
+  calculator.op1 = null;
+  calculator.op2 = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+  console.log(calculator);
+}
+
+function reset() {
+  calculator.op1 = null;
+  calculator.op2 = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+}
+
+function equalOperator() {
+  const { op1, waitingForSecondOperand, secondOperand, displayValue, operator } = calculator;
+  if ((waitingForSecondOperand === true && secondOperand === "0") || waitingForSecondOperand === false) {
+    calculator.displayValue = parseFloat(displayValue);
+    reset();
+    console.log(calculator);
+  } else if (waitingForSecondOperand === true && secondOperand != "0") {
+    let secondInputValue = parseFloat(secondOperand);
+    calculator.op2 = secondInputValue;
+    const result = calculate(op1, calculator.op2, operator);
+    console.log(result);
+    calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+    reset();
+    calculator.secondOperand = "0";
+    console.log(calculator);
+  }
 }
 
 function calculate(op1, op2, operator) {
@@ -54,12 +106,11 @@ function calculate(op1, op2, operator) {
     return op1 + op2;
   } else if (operator === "-") {
     return op1 - op2;
-  } else if (operator === "*") {
+  } else if (operator === "x") {
     return op1 * op2;
-  } else if (operator === "/") {
+  } else if (operator === "÷") {
     return op1 / op2;
   }
-
   return op2;
 }
 
@@ -82,18 +133,34 @@ keys.addEventListener("click", (event) => {
     return;
   }
   if (target.classList.contains("decimal")) {
-    inputDecimal(target.value);
+    if (calculator.waitingForSecondOperand === true) {
+      SecondInputDecimal(target.value);
+      updateDisplay();
+    } else {
+      FirstInputDecimal(target.value);
+      updateDisplay();
+    }
+    return;
+  }
+  if (target.classList.contains("equal-sign")) {
+    equalOperator();
     updateDisplay();
     return;
   }
-  input(target.value);
-  updateDisplay();
+  if (calculator.waitingForSecondOperand === true) {
+    secondInput(target.value);
+    updateDisplay();
+  } else {
+    input(target.value);
+    updateDisplay();
+  }
 });
 
 const allClear = document.querySelector(".keypad");
 allClear.addEventListener("click", (event) => {
   const { target } = event;
   if (target.classList.contains("all-clear")) {
-    console.log("clear", target.value);
+    resetCalculator();
+    updateDisplay();
   }
 });
